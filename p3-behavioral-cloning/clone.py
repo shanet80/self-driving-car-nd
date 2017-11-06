8d4294918a35864b582e5c681e7b42858c65e078
Shane Thompson <shanet80@gmail.com>
2017-11-05 21:57:39 -0500

    Limit training epochs to 4 to reduce overfitting

--- p3-behavioral-cloning/clone.py
+++ p3-behavioral-cloning/clone.py
@@ -34,6 +34,6 @@ model.add(Flatten(input_shape=(160, 320, 3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=4)
 
 model.save('./p3-behavioral-cloning/model.h5')
